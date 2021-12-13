0719bb657d7130c97bebe427f5948a508100de3f
GromovvVadim <vadimgromov1403@gmail.com>
2021-12-13 19:27:57 +0200

    Added static files and service

---
+++ SST.Application/Common/Interfaces/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SST.Application.Common.Interfaces
+{
+    public interface IAccountService
+    {
+        public Task CreateStudentAccount(string email, string password, int studentId);
+
+        public Task CreateLectorAccount(string email, string password, int lectorId);
+
+        public Task<ClaimsPrincipal> Login(string email, string password);
+
+        public Task ChangePassword(string user, string oldpassword, string password);
+    }
+}
---
+++ SST.Application/DependencyModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Autofac;
+using MediatR;
+
+namespace SST.Application
+{
+    public class DependencyModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(
+                typeof(IMediator).GetTypeInfo().Assembly)
+                .AsImplementedInterfaces();
+
+            // builder.RegisterAssemblyTypes(typeof().GetTypeInfo().Assembly)
+        }
+    }
+}
